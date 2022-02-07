c4e3026064c1888478ad44eebe73091d4e4b42b9
Jon Esparaz <48423418+JonEsparaz@users.noreply.github.com>
2022-02-06 19:47:18 -0500

    Simple landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ReactComponent as Icon } from "./icon.svg";
+import { ReactComponent as Twitter } from "./twitter.svg";
+import { ReactComponent as Facebook } from "./facebook.svg";
+import { ReactComponent as Instagram } from "./instagram.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <div className="main-container">
+    <Icon width={50} height={50} />
+    <h1>Speedcubing Canada</h1>
+    <h3>More from us soon</h3>
+    <div className="social-links">
+      <a
+        href="https://twitter.com/SpeedcubingCAN"
+        title="twitter"
+        rel="noopener noreferrer"
+      >
+        <Twitter width={25} height={25} />
+      </a>
+      <a
+        href="https://www.facebook.com/speedcubingcan"
+        title="facebook"
+        rel="noopener noreferrer"
+      >
+        <Facebook width={25} height={25} />
+      </a>
+      <a
+        href="https://www.instagram.com/speedcubingcanada/"
+        title="instagram"
+        rel="noopener noreferrer"
+      >
+        <Instagram width={25} height={25} />
+      </a>
     </div>
-  );
-}
+  </div>
+);
 
 export default App;
