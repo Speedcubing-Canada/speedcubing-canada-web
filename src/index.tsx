6b6a1b5a855dbc8e7466df02139fac6978e99cf0
Jon Esparaz <48423418+JonEsparaz@users.noreply.github.com>
2022-04-24 22:48:24 -0400

    Revert "Import Montserrat via Fontsource"
    This reverts commit d8e149d061453a1fbf1337c33dd014e794d00aec.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import "@fontsource/montserrat";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
