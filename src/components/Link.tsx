fa18ba3112f8fa9799083ceff400259cd0d2a51b
Jon Esparaz <48423418+JonEsparaz@users.noreply.github.com>
2022-06-19 19:32:06 -0400

    Fix React ref MUI issue
    https://mui.com/material-ui/guides/composition/#caveat-with-refs

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,10 +1,17 @@
+import { forwardRef } from "react";
+
 export interface LinkProps {
   to: string;
   children?: React.ReactNode;
 }
 
-export const Link = ({ to, children, ...rest }: LinkProps) => (
-  <a href={to} target="_blank" rel="noopener noreferrer" {...rest}>
-    {children}
-  </a>
+export const Link = forwardRef(
+  (
+    { to, children, ...rest }: LinkProps,
+    ref: React.ForwardedRef<HTMLAnchorElement>
+  ) => (
+    <a ref={ref} href={to} target="_blank" rel="noopener noreferrer" {...rest}>
+      {children}
+    </a>
+  )
 );
