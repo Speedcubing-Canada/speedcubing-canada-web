f53e1fcb03131ead788dc087f7ad8aaefb4d4414
Alexandre Ondet <alexandre.ondet63@gmail.com>
2023-05-23 17:23:03 -0400

    starting auth based on what scramble-matcher is doing

---
+++ app/src/logic/auth.ts
@@ -0,0 +1,35 @@
+import { WCA_ORIGIN, WCA_OAUTH_CLIENT_ID } from './wca-env';
+
+/**
+ * Checks the URL hash for presence of OAuth access token
+ * and return it if it's found.
+ * Should be called on application initialization (before any kind of router takes over the location).
+ */
+export const getOauthTokenIfAny = () => {
+  const hash = window.location.hash.replace(/^#/, '');
+  const hashParams = new URLSearchParams(hash);
+  if (hashParams.has('access_token')) {
+    window.location.hash = '';
+    return hashParams.get('access_token');
+  }
+  return null;
+};
+
+export const signIn = () => {
+  const params = new URLSearchParams({
+    client_id: WCA_OAUTH_CLIENT_ID,
+    response_type: 'token',
+    redirect_uri: oauthRedirectUri(),
+    scope: 'public manage_competitions',
+  });
+  const win: Window = window;
+  win.location = `${WCA_ORIGIN}/oauth/authorize?${params.toString()}`;
+};
+
+const oauthRedirectUri = () => {
+  const { origin, pathname, search } = window.location;
+  const searchParams = new URLSearchParams(search);
+  const staging = searchParams.get('staging');
+  const appUri = `${origin}${pathname}`.replace(/\/$/, '');
+  return staging ? `${appUri}?staging=true` : appUri;
+};
--- app/src/pages/Account.tsx
+++ app/src/pages/Account.tsx
@@ -2,6 +2,8 @@ import { useTranslation } from "react-i18next";
 import {
     Container,
 } from "@mui/material";
+import Button from '@mui/material/Button';
+import { signIn } from '../logic/auth';
 
 export const Account = () => {
     const { t } = useTranslation();
@@ -9,6 +11,14 @@ export const Account = () => {
   return (
     <Container maxWidth="md">
         <div>Account</div>
+        <Button
+        variant="outlined"
+        component="span"
+        
+        onClick={signIn}
+      >
+        Sign in with the WCA
+      </Button>
     </Container>
   );
 };
