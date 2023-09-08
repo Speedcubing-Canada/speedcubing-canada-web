3534af6d6b744dc86516786067c99aff4de57eac
Alexandre Ondet <alexandre.ondet63@gmail.com>
2023-09-07 23:34:14 -0400

    formating that works :)

--- app/src/pages/Quebec.tsx
+++ app/src/pages/Quebec.tsx
@@ -1,24 +1,20 @@
-import {useTranslation} from "react-i18next";
-import {Box, Container, Typography} from "@mui/material";
-import {Link} from "../components/Link";
-import {LINKS} from "./links";
+import { useTranslation } from "react-i18next";
+import { Box, Container, Typography } from "@mui/material";
+import { Link } from "../components/Link";
+import { LINKS } from "./links";
 export const Quebec = () => {
-        const {t} = useTranslation();
-        window.location.href = LINKS.DISCORD_QC;
+  const { t } = useTranslation();
+  window.location.href = LINKS.DISCORD_QC;
 
-        return (
-            <Container maxWidth="md">
-                <Box marginY="4rem">
-                    <Typography component="h1" variant="h3" fontWeight="bold" gutterBottom>
-                        {t("provinces.qc")}
-                    </Typography>
-                    <Typography gutterBottom>
-                        {t("quebec.body")}
-                    </Typography>
-                    <Link to={LINKS.DISCORD_QC}>Discord</Link>
-                </Box>
-            </Container>
-
-        );
-    }
-;
+  return (
+    <Container maxWidth="md">
+      <Box marginY="4rem">
+        <Typography component="h1" variant="h3" fontWeight="bold" gutterBottom>
+          {t("provinces.qc")}
+        </Typography>
+        <Typography gutterBottom>{t("quebec.body")}</Typography>
+        <Link to={LINKS.DISCORD_QC}>Discord</Link>
+      </Box>
+    </Container>
+  );
+};
