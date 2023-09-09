a5d40e8e9cfa34f25e70800fdb09b075518e72b2
Nicholas McKee <nmsmckee@gmail.com>
2023-09-09 19:48:25 -0400

    Minor refactor to fix coding style

--- app/src/components/Base.tsx
+++ app/src/components/Base.tsx
@@ -14,8 +14,7 @@ import {
   IconButton,
   Drawer,
 } from "@mui/material";
-import { Home, Info, CorporateFare, QuestionAnswer } from "@mui/icons-material";
-import MenuIcon from '@mui/icons-material/Menu';
+import { Home, Info, CorporateFare, QuestionAnswer, Menu } from "@mui/icons-material";
 import { Link, Outlet, useLocation, useParams } from "react-router-dom";
 import { getLocaleOrFallback, SAVED_LOCALE } from "../locale";
 
@@ -35,12 +34,17 @@ const ROUTE_NAME_TO_ROUTE = {
   faq: "faq",
 } as const;
 
+const BREAKPOINT = 700;
+
 export const Base = () => {
   const { t } = useTranslation();
   const { pathname } = useLocation();
   const pathWithoutLocale = pathname.split("/").at(-1);
   const params = useParams();
   const locale = getLocaleOrFallback(params.locale as string);
+
+  const [width, setWidth] = React.useState(window.innerWidth);
+  const [drawerState, setDrawerState] = useState(false);
 
   useEffect(() => {
     localStorage.setItem(SAVED_LOCALE, locale);
@@ -51,53 +55,49 @@ export const Base = () => {
     window.scrollTo(0, 0);
   }, [pathWithoutLocale]);
 
-  const [width, setWidth] = React.useState(window.innerWidth);
-  const breakpoint = 700;
-
-  const [drawerState, setDrawerState] = useState(false);
-
   useEffect(() => {
     window.addEventListener("resize", () => setWidth(window.innerWidth));
   } , []);
 
   return (
-    ( width > breakpoint
-      ? <Box minHeight="100vh" flex={1} display="flex" flexDirection="column">
-          <Box display="flex" flex={1}>
-            <Outlet />
-          </Box>
-          <Paper
-            sx={{ position: "sticky", bottom: 0, left: 0, right: 0, zIndex: 1100 }}
-            elevation={2}
-          >
-            <BottomNavigation showLabels value={pathWithoutLocale}>
-              {ROUTES.map((r) => {
-                const Icon = ICONS[r];
-                const route = ROUTE_NAME_TO_ROUTE[r];
-                const routeWithLocale = `${locale}/${route}`;
-
-                return (
-                  <BottomNavigationAction
-                    key={r}
-                    label={t(`routes.${r}`)}
-                    icon={<Icon />}
-                    to={routeWithLocale}
-                    value={route}
-                    component={Link}
-                  />
-                );
-              })}
-            </BottomNavigation>
-          </Paper>
+    ( width > BREAKPOINT
+      ?
+      <Box minHeight="100vh" flex={1} display="flex" flexDirection="column">
+        <Box display="flex" flex={1}>
+          <Outlet />
         </Box>
+        <Paper
+          sx={{ position: "sticky", bottom: 0, left: 0, right: 0, zIndex: 1100 }}
+          elevation={2}
+        >
+          <BottomNavigation showLabels value={pathWithoutLocale}>
+            {ROUTES.map((r) => {
+              const Icon = ICONS[r];
+              const route = ROUTE_NAME_TO_ROUTE[r];
+              const routeWithLocale = `${locale}/${route}`;
+
+              return (
+                <BottomNavigationAction
+                  key={r}
+                  label={t(`routes.${r}`)}
+                  icon={<Icon />}
+                  to={routeWithLocale}
+                  value={route}
+                  component={Link}
+                />
+              );
+            })}
+          </BottomNavigation>
+        </Paper>
+      </Box>
       : 
       <Box minHeight="90vh" flex={1} display="flex" flexDirection="column"> 
         <Paper
-          sx={{ position: "sticky", top: 0, zIndex: 1000 }}
+          sx={{ position: "sticky", top: 0, zIndex: 1100 }}
           elevation={2}
         >
           <IconButton onClick={() => setDrawerState(true)}>
-            <MenuIcon sx={{ fontSize: 40, color: "black" }} />
+            <Menu sx={{ fontSize: 40, color: "black" }} />
           </IconButton>
         </Paper>
         <Drawer
@@ -115,7 +115,7 @@ export const Base = () => {
                 <ListItem key={r} disablePadding>
                   <ListItemButton component={Link} to={routeWithLocale} onClick={() => setDrawerState(false)}>
                     <ListItemIcon>
-                      {<Icon />}
+                      <Icon />
                     </ListItemIcon>
                     <ListItemText primary={t(`routes.${r}`)} />
                   </ListItemButton>
