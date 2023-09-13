8d463570319fe58139fb24ad7b419ad589ec89bd
Alexandre Ondet <alexandre.ondet63@gmail.com>
2023-09-12 22:26:17 -0400

    better URL handling

--- app/src/components/api.ts
+++ app/src/components/api.ts
@@ -4,9 +4,9 @@ export const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL || "https://api.speedcubingcanada.org";
 
 export const signIn = () => {
-  window.location.assign(API_BASE_URL + "/login");
+  window.location.assign(new URL("/login", API_BASE_URL));
 };
 
 export const signOut = () => {
-  window.location.assign(API_BASE_URL + "/logout");
+  window.location.assign(new URL("/logout", API_BASE_URL));
 };
