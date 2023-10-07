2ca82747eff68f36a508c279b011fddd4bc2a8ad
Nicholas McKee <nmsmckee@gmail.com>
2023-10-06 23:03:38 -0400

    refactored with prettier

--- app/src/components/Base.tsx
+++ app/src/components/Base.tsx
@@ -16,7 +16,13 @@ import {
   useMediaQuery,
   Theme,
 } from "@mui/material";
-import { Home, Info, CorporateFare, QuestionAnswer, Menu } from "@mui/icons-material";
+import {
+  Home,
+  Info,
+  CorporateFare,
+  QuestionAnswer,
+  Menu,
+} from "@mui/icons-material";
 import { Link, Outlet, useLocation, useParams } from "react-router-dom";
 import { getLocaleOrFallback, SAVED_LOCALE } from "../locale";
 
@@ -55,75 +61,73 @@ export const Base = () => {
     window.scrollTo(0, 0);
   }, [pathWithoutLocale]);
 
-  return (
-    ( isSmall
-      ?
-      <Box minHeight="90vh" flex={1} display="flex" flexDirection="column"> 
-        <Paper
-          sx={{ position: "sticky", top: 0, zIndex: 1100 }}
-          elevation={2}
-        >
-          <IconButton onClick={() => setDrawerState(true)}>
-            <Menu sx={{ fontSize: 40, color: "black" }} />
-          </IconButton>
-        </Paper>
-        <Drawer
-          open={drawerState}
-          anchor="left"
-          onClose={() => setDrawerState(false)}
-        >
-          <List>
-            {ROUTES.map((r) => {
-              const Icon = ICONS[r];
-              const route = ROUTE_NAME_TO_ROUTE[r];
-              const routeWithLocale = `${locale}/${route}`;
+  return isSmall ? (
+    <Box minHeight="90vh" flex={1} display="flex" flexDirection="column">
+      <Paper sx={{ position: "sticky", top: 0, zIndex: 1100 }} elevation={2}>
+        <IconButton onClick={() => setDrawerState(true)}>
+          <Menu sx={{ fontSize: 40, color: "black" }} />
+        </IconButton>
+      </Paper>
+      <Drawer
+        open={drawerState}
+        anchor="left"
+        onClose={() => setDrawerState(false)}
+      >
+        <List>
+          {ROUTES.map((r) => {
+            const Icon = ICONS[r];
+            const route = ROUTE_NAME_TO_ROUTE[r];
+            const routeWithLocale = `${locale}/${route}`;
 
-              return (
-                <ListItem key={r} disablePadding>
-                  <ListItemButton component={Link} to={routeWithLocale} onClick={() => setDrawerState(false)}>
-                    <ListItemIcon>
-                      <Icon />
-                    </ListItemIcon>
-                    <ListItemText primary={t(`routes.${r}`)} />
-                  </ListItemButton>
-                </ListItem>
-              );
-            })}
-          </List>
-        </Drawer>
-        <Box display="flex" flex={1}>
-          <Outlet />
-        </Box> 
-      </Box>
-      :
-      <Box minHeight="100vh" flex={1} display="flex" flexDirection="column">
-        <Box display="flex" flex={1}>
-          <Outlet />
-        </Box>
-        <Paper
-          sx={{ position: "sticky", bottom: 0, left: 0, right: 0, zIndex: 1100 }}
-          elevation={2}
-        >
-          <BottomNavigation showLabels value={pathWithoutLocale}>
-            {ROUTES.map((r) => {
-              const Icon = ICONS[r];
-              const route = ROUTE_NAME_TO_ROUTE[r];
-              const routeWithLocale = `${locale}/${route}`;
-
-              return (
-                <BottomNavigationAction
-                  key={r}
-                  label={t(`routes.${r}`)}
-                  icon={<Icon />}
-                  to={routeWithLocale}
-                  value={route}
+            return (
+              <ListItem key={r} disablePadding>
+                <ListItemButton
                   component={Link}
-                />
-              );
-            })}
-          </BottomNavigation>
-        </Paper>
-      </Box> 
-    )
+                  to={routeWithLocale}
+                  onClick={() => setDrawerState(false)}
+                >
+                  <ListItemIcon>
+                    <Icon />
+                  </ListItemIcon>
+                  <ListItemText primary={t(`routes.${r}`)} />
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
+        </List>
+      </Drawer>
+      <Box display="flex" flex={1}>
+        <Outlet />
+      </Box>
+    </Box>
+  ) : (
+    <Box minHeight="100vh" flex={1} display="flex" flexDirection="column">
+      <Box display="flex" flex={1}>
+        <Outlet />
+      </Box>
+      <Paper
+        sx={{ position: "sticky", bottom: 0, left: 0, right: 0, zIndex: 1100 }}
+        elevation={2}
+      >
+        <BottomNavigation showLabels value={pathWithoutLocale}>
+          {ROUTES.map((r) => {
+            const Icon = ICONS[r];
+            const route = ROUTE_NAME_TO_ROUTE[r];
+            const routeWithLocale = `${locale}/${route}`;
+
+            return (
+              <BottomNavigationAction
+                key={r}
+                label={t(`routes.${r}`)}
+                icon={<Icon />}
+                to={routeWithLocale}
+                value={route}
+                component={Link}
+              />
+            );
+          })}
+        </BottomNavigation>
+      </Paper>
+    </Box>
   );
 };
