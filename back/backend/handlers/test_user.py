1b52ec4de5a6cb02966da5465e2f8764cba36667
Alexandre Ondet <alexandre.ondet63@gmail.com>
2023-12-09 21:56:55 -0500

    all tests for user_info are done

--- back/backend/handlers/test_user.py
+++ back/backend/handlers/test_user.py
@@ -6,6 +6,7 @@ from backend.lib.secrets import get_secret
 from backend.test.mock_ndb_client import ndb_client
 from backend.test.mock_user import TestUser
 
+
 @pytest.fixture()
 def app(mocker):
     mocker.patch("backend.flask.get_secret", return_value="testing")
@@ -18,12 +19,13 @@ def client(app):
     return app.test_client()
 
 
-def test_user_info(client):
+def test_user_info_401(client):
     response = client.get("/user_info")
-    assert b'{"error":"Unauthorized"}\n' in response.data
+    assert response.json["error"] == "Unauthorized"
+    assert response.status_code == 401
 
 
-def test_modify_session(client, mocker):
+def test_user_info_own_info(client, mocker):
     id = 46689
     mocker.patch("google.cloud.ndb.model.Model.get_by_id", return_value=TestUser(id))
     with client.session_transaction() as session:
@@ -32,3 +34,41 @@ def test_modify_session(client, mocker):
     response = client.get("/user_info")
     google.cloud.ndb.model.Model.get_by_id.assert_called_with(46689)
     assert response.json["id"] == 46689
+    assert response.json["name"] == "Test User"
+    assert response.json["roles"] == []
+    assert response.json["dob"] == "01-01-2000"
+    assert response.json["province"] == "QC"
+    assert response.json["wca_person"] == "2020XXXX01"
+    assert response.json["email"] == "test@test.com"
+
+
+def test_user_info_403(client, mocker):
+    mocker.patch("google.cloud.ndb.model.Model.get_by_id", side_effect=lambda x: TestUser(x))
+    with client.session_transaction() as session:
+        session["wca_account_number"] = 46689
+    response = client.get("/user_info/123")
+    assert response.json["error"] == "You're not authorized to view this user."
+    assert response.status_code == 403
+
+
+def test_user_info_as_delegate(client, mocker):
+    mocker.patch("google.cloud.ndb.model.Model.get_by_id", side_effect=lambda x: TestUser(x))
+    with client.session_transaction() as session:
+        session["wca_account_number"] = 1
+    response = client.get("/user_info/123")
+    assert response.json["id"] == "123"
+    assert response.json["name"] == "Test User"
+    assert response.json["roles"] == []
+    assert response.json["dob"] == "01-01-2000"
+    assert response.json["province"] == "QC"
+    assert response.json["wca_person"] == "2020XXXX01"
+    assert response.json["email"] == "test@test.com"
+
+
+def test_user_info_404(client, mocker):
+    mocker.patch("google.cloud.ndb.model.Model.get_by_id", side_effect=(lambda x: None if x == "100" else TestUser(x)))
+    with client.session_transaction() as session:
+        session["wca_account_number"] = 1
+    response = client.get("/user_info/100")
+    assert response.json["error"] == "Unrecognized user ID 100"
+    assert response.status_code == 404
--- back/backend/test/mock_user.py
+++ back/backend/test/mock_user.py
@@ -1,5 +1,5 @@
 class TestUser:
-    def __init__(self, id, wca_person="2020XXXX01", name="test_user", email="test@test.com", dob="01-01-2000", roles=[],
+    def __init__(self, id, wca_person="2020XXXX01", name="Test User", email="test@test.com", dob="01-01-2000", roles=[],
                  province="QC"):
         self.id = id
         self.wca_person = wca_person
@@ -8,6 +8,8 @@ class TestUser:
         self.dob = dob
         self.roles = roles
         self.province = province
+        if id == 1:
+            self.roles = ["DELEGATE"]  # This is a way to test for a user that has more rights.
 
     def to_json(self):
         return {
@@ -19,3 +21,9 @@ class TestUser:
             "wca_person": self.wca_person,
             "email": self.email,
         }
+
+    def has_any_of_given_roles(self, roles):
+        for role in self.roles:
+            if role in roles:
+                return True
+        return False
