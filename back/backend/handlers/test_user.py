971b6dd6fd9fea42d642f0817769137a372807a7
Alexandre Ondet <alexandre.ondet63@gmail.com>
2023-12-10 20:12:00 -0500

    starting to test /edit

--- back/backend/handlers/test_user.py
+++ back/backend/handlers/test_user.py
@@ -1,9 +1,11 @@
+from unittest.mock import MagicMock
+
 import google
 import pytest
 
 from backend.flask import create_app
 from backend.test.mock_ndb_client import ndb_client
-from backend.test.mock_user import TestUser
+from backend.test.mock_user import TestUser, TestProvince, TestUserLocationUpdate, testUpdate
 
 
 @pytest.fixture()
@@ -11,6 +13,11 @@ def app(mocker):
     mocker.patch("backend.flask.get_secret", return_value="testing")
     mocker.patch("google.cloud.ndb.Client", return_value=ndb_client)
     mocker.patch("google.cloud.ndb.model.Model.get_by_id", side_effect=(lambda x: None if x == "100" else TestUser(x)))
+    mocker.patch("google.cloud.ndb.Key", side_effect=(lambda x, y: TestProvince(y, x)))
+    mocker.patch("google.cloud.ndb.Key.get", side_effect=(lambda x: x))
+    mocker.patch("google.cloud.ndb.model.Model.put")
+    mocker.patch("backend.handlers.user.rewrite_ranks")
+    mocker.patch("backend.handlers.user.UserLocationUpdate", return_value=testUpdate)
     return create_app()
 
 
@@ -18,12 +25,14 @@ def app(mocker):
 def client(app):
     return app.test_client()
 
+
 @pytest.fixture()
 def client_as_normal_user(client):
     with client.session_transaction() as session:
         # set a user id without going through the login route
         session["wca_account_number"] = 2
     return client
+
 
 @pytest.fixture()
 def client_as_delegate(client):
@@ -38,7 +47,7 @@ def test_user_info_401(client):
     assert response.status_code == 401
 
 
-def test_user_info_own_info(client_as_normal_user, mocker):
+def test_user_info_own_info(client_as_normal_user):
     response = client_as_normal_user.get("/user_info")
     google.cloud.ndb.model.Model.get_by_id.assert_called_with(2)
     assert response.json["id"] == 2
@@ -50,13 +59,13 @@ def test_user_info_own_info(client_as_normal_user, mocker):
     assert response.json["email"] == "test@test.com"
 
 
-def test_user_info_403(client_as_normal_user, mocker):
+def test_user_info_403(client_as_normal_user):
     response = client_as_normal_user.get("/user_info/123")
     assert response.json["error"] == "You're not authorized to view this user."
     assert response.status_code == 403
 
 
-def test_user_info_as_delegate(client_as_delegate, mocker):
+def test_user_info_as_delegate(client_as_delegate):
     response = client_as_delegate.get("/user_info/123")
     assert response.json["id"] == "123"
     assert response.json["name"] == "Test User"
@@ -67,7 +76,24 @@ def test_user_info_as_delegate(client_as_delegate, mocker):
     assert response.json["email"] == "test@test.com"
 
 
-def test_user_info_404(client_as_delegate, mocker):
+def test_user_info_404(client_as_delegate):
     response = client_as_delegate.get("/user_info/100")
     assert response.json["error"] == "Unrecognized user ID 100"
     assert response.status_code == 404
+
+
+def test_edit_401(client):
+    response = client.post("/edit")
+    assert response.json["error"] == "Unauthorized"
+    assert response.status_code == 401
+
+
+def test_edit_own_info(client_as_normal_user):
+    response = client_as_normal_user.post("/edit", json={"province": "ON"})
+    assert response.json["id"] == 2
+    assert response.json["name"] == "Test User"
+    assert response.json["roles"] == []
+    assert response.json["dob"] == "01-01-2000"
+    assert response.json["province"] == "ON"
+    assert response.json["wca_person"] == "2020XXXX01"
+    assert response.json["email"] == "test@test.com"
