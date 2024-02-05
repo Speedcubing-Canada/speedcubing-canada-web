1f1f221ff499b6bbbcad2f9b3688d9780c2bb880
Jon Esparaz <48423418+JonEsparaz@users.noreply.github.com>
2024-02-04 22:37:22 -0500

    Remove Brady Metherall from list of Directors

--- app/src/pages/Organization.tsx
+++ app/src/pages/Organization.tsx
@@ -19,7 +19,6 @@ const DIRECTORS = [
   { name: "Kristopher De Asis", wcaId: "2008ASIS01" },
   { name: "Jonathan Esparaz", wcaId: "2013ESPA01" },
   { name: "Abdullah Gulab", wcaId: "2014GULA02" },
-  { name: "Brady Metherall", wcaId: "2009METH01" },
   { name: "Liam Orovec", wcaId: "2014OROV01" },
 ] as const;
 
