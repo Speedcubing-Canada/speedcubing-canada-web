1bc85f989d9dadd4acc72fa1e573198ec90d3fa1
Alexandre Ondet <61289458+AlexandreOndet@users.noreply.github.com>
2025-07-30 18:41:47 -0400

    updated tutorials for the french version of the website (#89)

--- app/src/pages/FAQ.tsx
+++ app/src/pages/FAQ.tsx
@@ -12,28 +12,36 @@ const QUESTIONS = [
   "why-the-change-from-canadiancubing-to-speedcubing-canada",
 ] as const;
 
-const INTERPOLATE = {
-  "when-is-the-next-wca-competition-in-my-area": {
-    wcaComps: <Link to={LINKS.WCA.COMPS_CANADA} />,
-    mailingList: <Link to={LINKS.MAILING_LIST} />,
-  },
-  "im-going-to-my-first-wca-competition-what-do-i-need-to-know": {
-    regs: <Link to={LINKS.WCA.REGS} />,
-    firstComp: <Link to={LINKS.FIRST_COMP} />,
-    compBasics: <Link to={LINKS.COMP_BASICS} />,
-  },
-  "who-are-the-wca-delegates-in-my-area": {
-    delegates: <Link to={LINKS.WCA.DELEGATES} />,
-  },
-  "how-can-i-volunteer-with-speedcubing-canada": {},
-  "affiliated-with-the-wca": {
-    regionalOrg: <Link to={LINKS.WCA.REGIONAL_ORGS} />,
-  },
-  "why-the-change-from-canadiancubing-to-speedcubing-canada": {},
-} as const;
-
 export const FAQ = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+
+  const INTERPOLATE = {
+    "when-is-the-next-wca-competition-in-my-area": {
+      wcaComps: <Link to={LINKS.WCA.COMPS_CANADA} />,
+      mailingList: <Link to={LINKS.MAILING_LIST} />,
+    },
+    "im-going-to-my-first-wca-competition-what-do-i-need-to-know": {
+      regs: <Link to={LINKS.WCA.REGS} />,
+      firstComp: (
+        <Link
+          to={i18n.language === "fr" ? LINKS.FIRST_COMP_FR : LINKS.FIRST_COMP}
+        />
+      ),
+      compBasics: (
+        <Link
+          to={i18n.language === "fr" ? LINKS.COMP_BASICS_FR : LINKS.COMP_BASICS}
+        />
+      ),
+    },
+    "who-are-the-wca-delegates-in-my-area": {
+      delegates: <Link to={LINKS.WCA.DELEGATES} />,
+    },
+    "how-can-i-volunteer-with-speedcubing-canada": {},
+    "affiliated-with-the-wca": {
+      regionalOrg: <Link to={LINKS.WCA.REGIONAL_ORGS} />,
+    },
+    "why-the-change-from-canadiancubing-to-speedcubing-canada": {},
+  } as const;
 
   return (
     <Container maxWidth="md">
